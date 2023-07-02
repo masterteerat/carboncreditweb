64d64200ab22ff3a934c9b54cb8beecc0be89962
masterteerat <teerat.saetan@gmail.com>
2023-07-02 19:20:58 +0700

    first commit

---
+++ signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+
+
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="sign-up">
+            <div class="logo">
+                <img src="HTMLTEXT.HTML/pic/Free-Emblem" alt="">
+            </div>
+            <div class="title">RCCS</div>
+            <div class="title">Retro carbon credit service</div>
+
+            <div class="form">
+                <div class="username">
+                    <input type="text" placeholder="Username">
+                </div>
+                <div class="Email">
+                    <input type="text" placeholder="Email">
+                </div>
+                <div class="creditcard id">
+                    <input type="number" placeholder="id">
+                </div>
+                <div class="password">
+                    <input type="password" placeholder="Password">
+                </div>
+            </div>
+
+            
+            
+            <button class="sign-btn">Login</button>
+           
+            
+
+        </div>
+
+    </body>
+
+
+</html>
