010ad622ea93e3d7f01f9ae272973ee8bf385b18
masterteerat <teerat.saetan@gmail.com>
2023-07-08 15:47:29 +0700

    Add main and user input error handling

---
+++ main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+
+
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="login">
+            <a href="login.php">Login</a>
+        </div>
+        <div class="sign-up">
+            <a href="signup.php">Sign-up</a>
+        </div>
+
+    </body>
+
+
+
+</html>
