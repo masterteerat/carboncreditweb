1b2df089c5ddccd649d209104a1bdbf9eff52cf6
masterteerat <teerat.saetan@gmail.com>
2023-07-09 11:08:01 +0700

    add js rm php form validation

--- register.php
+++ register.php
@@ -3,32 +3,47 @@ $servername = "db";
 $dbusername = "user";
 $dbpassword = "test";
 $dbname = "myDb";
+$error_massage = "Test error massage";  
+
+$input = FALSE;  
 
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $username = $_POST['username'];
-  $cardid = $_POST['cardid'];
-  $email = $_POST['email'];
-  $occupation = $_POST['occupation'];
-  $password = $_POST['password'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$cardid = $_POST['cardid'];
+$occupation = $_POST['occupation'];
+$password = $_POST['password'];
 
 
-}
+if($input == TRUE){
+  $sql = "INSERT INTO user (username, cardid, email, occupation, password)
+  VALUES ('$username','$cardid','$email', '$occupation', '$password')";
 
-$sql = "INSERT INTO user (username, cardid, email, occupation, password)
-VALUES ('$username','$cardid','$email', '$occupation', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
+  if ($conn->query($sql) === TRUE) {
+      //echo "New record created successfully";
+      header("Location: main.php");
+      exit(0);
+   
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
   
-  $conn->close();
+    $conn->close();
+} else {
+   echo
+  '<body onload="document.redirectform.submit()">   
+      <form method="POST" action="signup.php" name="redirectform" style="display:none">
+      <input name="username" value=' . $username. '>
+      <input name="cardid" value=' . $cardid. '>
+      <input name="email" value=' . $email. '>
+      <input name="occupation" value=' . $occupation. '>
+      <input name="password" value=' . $password. '>
+      <input name="error_massage" value="' .$error_massage. '">
+      </form>
+  </body>'; 
+}
 ?>
--- signup.php
+++ signup.php
@@ -1,13 +1,44 @@
 <!DOCTYPE html>
 
+<?php
+ 
+$error_masssage = $_POST['error_massage']; 
+?>
 
 
 <html>
     <head>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet" >
+
     </head>
+    <script>  
+    function verifyPassword() {  
+    var pw = document.getElementById("password").value;  
+    //check empty password field  
+    if(pw == "") {  
+        document.getElementById("message").innerHTML = "**Fill the password please!";  
+        return false;  
+    }  
+    
+    //minimum password length validation  
+    if(pw.length < 8) {  
+        document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+        return false;  
+    }  
+    
+    //maximum length of password validation  
+    if(pw.length > 15) {  
+        document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+        return false;  
+    } 
+
+    return true;
+
+    }  
+    </script>  
+
     <body>
         <div class="sign-up">
             <div class="logo">
@@ -15,31 +46,38 @@
             </div>
             <div class="title">RCCS</div>
             <div class="title">Retro carbon credit service</div>
-
-            <form class="form" action="register.php" method="post">
+            <?php
+                echo 
+                '
+                <div class="title">' .$error_masssage. '</div>
+                '
+                
+                ?>
+            <form class="form" action="register.php" method="post" onsubmit ="return verifyPassword()">
                 <div class="username">
                     <input type="text" placeholder="Username" name="username">
                 </div>
+
                 <div class="Email">
                     <input type="text" placeholder="Email" name="email">
                 </div>
-                <div class="creditcard id">
+                <div class="cardid">
                     <input type="text" placeholder="id" name="cardid">
                 </div>
+
                 <div class="occupation">
                     <input type="text" placeholder="Occupation or buisness" name="occupation">
-                </div>   
+                </div>  
                 <div class="password">
-                    <input type="password" placeholder="Password" name="password">
+                    <input type="password" placeholder="Password" name="password" id="password">
                 </div>
+                <span id = "message" style="color:red" value=""> </span> <br><br>
+
                 <input type="submit" value="Submit">
+
             </form>
 
-            
-            
-           
-            
-
+ 
         </div>
 
     </body>
